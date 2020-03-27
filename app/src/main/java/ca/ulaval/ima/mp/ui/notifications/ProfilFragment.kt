1afbc815085a2b71f5c9fd3579370b1129e8919a
Laurent Drolet <laurent.drolet.1@ulaval.ca>
2020-03-27 12:05:57 -0400

    Ajout de la NavBar

---
+++ app/src/main/java/ca/ulaval/ima/mp/ui/notifications/ProfilFragment.kt
@@ -0,0 +1,21 @@
+package ca.ulaval.ima.mp.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ca.ulaval.ima.mp.R
+
+class ProfilFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_profil, container, false)
+
+        return root
+    }
+}
