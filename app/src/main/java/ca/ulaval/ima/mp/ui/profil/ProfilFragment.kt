1c59372ddda5a671d780f48ddce4d742d9a9d15b
Laurent Drolet <laurent.drolet.1@ulaval.ca>
2020-04-01 16:39:27 -0400

    Création du background pour le formulaire de profil

--- app/src/main/java/ca/ulaval/ima/mp/ui/profil/ProfilFragment.kt
+++ app/src/main/java/ca/ulaval/ima/mp/ui/profil/ProfilFragment.kt
@@ -1,9 +1,11 @@
 package ca.ulaval.ima.mp.ui.profil
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import ca.ulaval.ima.mp.R
 
