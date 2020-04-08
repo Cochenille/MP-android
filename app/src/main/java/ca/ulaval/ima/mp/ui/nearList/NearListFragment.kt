4390555cec6c234131a65da5ffcb48b9713801b1
Samuel Bouchard <samuel.bouchard.7@ulaval.ca>
2020-04-08 11:53:38 -0400

    Added NearList fragment

--- app/src/main/java/ca/ulaval/ima/mp/ui/nearList/NearListFragment.kt
+++ app/src/main/java/ca/ulaval/ima/mp/ui/nearList/NearListFragment.kt
@@ -5,10 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ca.ulaval.ima.mp.ApiHelper
 import ca.ulaval.ima.mp.R
+import ca.ulaval.ima.mp.domain.Restaurant
+import okhttp3.Response
+import org.json.JSONException
+import org.json.JSONObject
 
 class NearListFragment : Fragment() {
-
+    private lateinit var recycledView: RecyclerView
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: RestaurantsRecyclerViewAdapter
+    private var apiHelper: ApiHelper = ApiHelper()
+    private var restaurantArray = ArrayList<Restaurant>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,7 +27,47 @@ class NearListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_nearlist, container, false)
+        recycledView = root.findViewById(R.id.restaurants_recycleView)
 
+        recycledView.isNestedScrollingEnabled = true
+
+        layoutManager = LinearLayoutManager(this.context)
+        recycledView.layoutManager = layoutManager
+        getRestaurants()
         return root
+    }
+
+    private fun getRestaurants() {
+        //TODO: Replace this position by dynamic postion
+        apiHelper.getRestaurantsWithinRadius(
+            46.781918,
+            -71.274810,
+            30,
+            object : ApiHelper.HttpCallback {
+                override fun onFailure(
+                    response: Response?,
+                    throwable: Throwable?
+                ) {
+                }
+
+                override fun onSuccess(response: Response?) {
+                    restaurantArray.clear()
+                    try {
+                        val jsonResponse = JSONObject(response?.body()!!.string())
+                        val content = jsonResponse.getJSONObject("content")
+                        val restaurantsJSONArray = content.getJSONArray("results")
+                        for (i in 0 until restaurantsJSONArray.length()) {
+                            val restaurantJson = restaurantsJSONArray.getJSONObject(i)
+                            val restaurant: Restaurant =
+                                Restaurant.fromJson(restaurantJson.toString())!!
+                            restaurantArray.add(restaurant)
+                        }
+                        adapter = RestaurantsRecyclerViewAdapter(restaurantArray)
+                        recycledView.adapter = adapter
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                }
+            })
     }
 }
---
+++ app/src/main/java/ca/ulaval/ima/mp/ui/nearList/RestaurantsRecyclerViewAdapter.kt
@@ -0,0 +1,78 @@
+package ca.ulaval.ima.mp.ui.nearList
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ca.ulaval.ima.mp.R
+import ca.ulaval.ima.mp.domain.Restaurant
+import com.squareup.picasso.Picasso
+import java.util.*
+
+
+class RestaurantsRecyclerViewAdapter(myDataset: ArrayList<Restaurant>) :
+    RecyclerView.Adapter<RestaurantsRecyclerViewAdapter.ViewHolder>() {
+    private val mDataset: ArrayList<Restaurant>
+    private var onItemClickListener: OnItemClickListener? = null
+
+    class ViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+        var restauranNameTextView: TextView
+        var imageView: ImageView
+        var distanceTextView: TextView
+        var ratingBar: RatingBar
+        var ratingCountTextView: TextView
+
+        init {
+            restauranNameTextView = view.findViewById(R.id.restaurant_name_textview)
+            imageView = view.findViewById(R.id.restaurant_image_view)
+            distanceTextView = view.findViewById(R.id.distance_textiview)
+            ratingBar = view.findViewById(R.id.stars_layout)
+            ratingCountTextView = view.findViewById(R.id.number_of_reviews_textview)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        // create a new view
+        val v: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.restaurant_infos_overlay, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        val restaurant: Restaurant = mDataset[position]
+        holder.restauranNameTextView.text = restaurant.name
+        Picasso.get().load(restaurant.image).into(holder.imageView)
+        holder.distanceTextView.text = String.format("%.1f km",restaurant.distance)
+        holder.ratingBar.rating = restaurant.reviewAverage.toFloat()
+        holder.ratingCountTextView.text = String.format("(%d)",restaurant.reviewCount)
+        val listener =
+            View.OnClickListener { onItemClickListener!!.onItemClick(restaurant) }
+        holder.view.setOnClickListener(listener)
+    }
+
+    override fun getItemCount(): Int {
+        return mDataset.size
+    }
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener?) {
+        this.onItemClickListener = onItemClickListener
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(item: Restaurant?)
+    }
+
+    init {
+        mDataset = myDataset
+    }
+}
